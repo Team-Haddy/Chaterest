c3ab4b01dcb49f76372af567f5644a2870b0ba60
Deepak Balaraman <deepak22deepak22@gmail.com>
2021-10-16 21:19:08 +0530

    Google Sign-in implementation

--- app/src/main/java/com/teamhaddy/app/chaterest/SplashScreenActivity.java
+++ app/src/main/java/com/teamhaddy/app/chaterest/SplashScreenActivity.java
@@ -5,13 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.teamhaddy.app.chaterest.ui.login.LoginActivity;
-
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, LoginActivity.class));
+        startActivity(new Intent(this, GoogleSignInActivity.class));
         finish();
     }
 }
