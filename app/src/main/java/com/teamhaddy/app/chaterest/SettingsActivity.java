e10b8d4daa55177c85661dd709a3bbfbfedcdc13
DHANUSH-6 <dhanushpowerk6@gmail.com>
2021-10-19 20:31:28 +0530

    settings linked to the interest page

--- app/src/main/java/com/teamhaddy/app/chaterest/SettingsActivity.java
+++ app/src/main/java/com/teamhaddy/app/chaterest/SettingsActivity.java
@@ -2,9 +2,12 @@ package com.teamhaddy.app.chaterest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,7 +16,6 @@ public class SettingsActivity extends AppCompatActivity {
     private Button updateSettings,ChangeInterest;
     private EditText Username;
     private CircleImageView UserProfilePic;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +30,24 @@ public class SettingsActivity extends AppCompatActivity {
         ChangeInterest= (Button) findViewById(R.id.set_profile_status);
         Username= (EditText) findViewById(R.id.set_username);
         UserProfilePic =(CircleImageView) findViewById(R.id.set_profile);
+    }
+
+
+    public void transition(View view) {
+        Intent interest =new Intent(this,Interest.class);
+        startActivity(interest);
+        Toast.makeText(getApplicationContext(), "Update Your Interest", Toast.LENGTH_SHORT).show();
+    }
+
+    public void update(View view) {
+        Button button = (Button) findViewById(R.id.update_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+        Intent interest =new Intent(this,MainActivity.class);
+        startActivity(interest);
+        Toast.makeText(getApplicationContext(), "Interest Updated Successfully", Toast.LENGTH_SHORT).show();
     }
 }
